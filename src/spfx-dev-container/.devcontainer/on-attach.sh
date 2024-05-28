34d24f4f45a1cb3c469d15c94c0e0e32b58b8f61
= <=>
2024-05-28 17:00:59 -0400

    Moved package restore to post attach.

---
+++ src/spfx-dev-container/.devcontainer/on-attach.sh
@@ -0,0 +1,3 @@
+echo
+echo -e "\e[1;94mInstalling Node dependencies\e[0m"
+npm install --no-audit
--- src/spfx-dev-container/.devcontainer/on-create.sh
+++ src/spfx-dev-container/.devcontainer/on-create.sh
@@ -1,7 +1,3 @@
-echo
-echo -e "\e[1;94mInstalling Node dependencies\e[0m"
-npm install --no-audit
-
 ## commands to create dev certificate and copy it to the root folder of the project
 echo
 echo -e "\e[1;94mGenerating dev certificate\e[0m"
